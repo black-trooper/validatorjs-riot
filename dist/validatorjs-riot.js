d42b5f0f823ed7e3290055cd72cc644f1387e788
black-trooper <hermanmiller.dkr@gmail.com>
2020-01-18 23:30:53 +0900

    Support es5

--- dist/validatorjs-riot.js
+++ dist/validatorjs-riot.js
@@ -103,133 +103,178 @@ return /******/ (function(modules) { // webpackBootstrap
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-const Validator = __webpack_require__(/*! validatorjs */ "validatorjs");
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-class ValidatorjsRiot extends Validator {
-  constructor(refs, option) {
-    const v = super({}, {});
-    const data = {};
-    const rules = {};
-    const attributeNames = {};
-    const customMessages = {};
-    this._option = option || {};
-    const fieldName = this._option.field_name || 'ref-label';
-    const customMessageAttributeName = this._option.custom_message_attribute_name || 'custom-message';
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    for (const name in refs) {
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var Validator = __webpack_require__(/*! validatorjs */ "validatorjs");
+
+var ValidatorjsRiot =
+/*#__PURE__*/
+function (_Validator) {
+  _inherits(ValidatorjsRiot, _Validator);
+
+  function ValidatorjsRiot(refs, option) {
+    var _this;
+
+    _classCallCheck(this, ValidatorjsRiot);
+
+    var v = _this = _possibleConstructorReturn(this, _getPrototypeOf(ValidatorjsRiot).call(this, {}, {}));
+
+    var data = {};
+    var rules = {};
+    var attributeNames = {};
+    var customMessages = {};
+    _this._option = option || {};
+    var fieldName = _this._option.field_name || 'ref-label';
+    var customMessageAttributeName = _this._option.custom_message_attribute_name || 'custom-message';
+
+    var _loop = function _loop(name) {
       if (!refs.hasOwnProperty(name)) {
-        continue;
+        return "continue";
       }
 
-      const ref = refs[name];
-      let attributes = ref.attributes;
+      var ref = refs[name];
+      var attributes = ref.attributes;
 
       if (!attributes && ref.root) {
         attributes = ref.root.attributes;
       }
 
-      if (!this._isTarget(name)) {
-        continue;
+      if (!_this._isTarget(name)) {
+        return "continue";
       }
 
-      data[name] = this._prepareData(ref.value, attributes.type);
-      rules[name] = this._prepareRule(attributes);
+      data[name] = _this._prepareData(ref.value, attributes.type);
+      rules[name] = _this._prepareRule(attributes);
 
       if (attributes[fieldName]) {
         attributeNames[name] = attributes[fieldName].nodeValue;
       }
 
       if (attributes[customMessageAttributeName]) {
-        const value = attributes[customMessageAttributeName].nodeValue;
+        var value = attributes[customMessageAttributeName].nodeValue;
 
         try {
-          const obj = JSON.parse(value);
-          Object.keys(obj).forEach(key => {
-            customMessages[`${key}.${name}`] = obj[key];
+          var obj = JSON.parse(value);
+          Object.keys(obj).forEach(function (key) {
+            customMessages["".concat(key, ".").concat(name)] = obj[key];
           });
         } catch (e) {
           if (value.indexOf(':') > 0) {
-            const array = value.split(':');
-            customMessages[`${array[0]}.${name}`] = array[1];
+            var array = value.split(':');
+            customMessages["".concat(array[0], ".").concat(name)] = array[1];
           } else {
-            customMessages[`regex.${name}`] = value;
+            customMessages["regex.".concat(name)] = value;
           }
         }
       }
+    };
+
+    for (var name in refs) {
+      var _ret = _loop(name);
+
+      if (_ret === "continue") continue;
     }
 
     v.input = data;
-    v.rules = super._parseRules(rules);
+    v.rules = _get(_getPrototypeOf(ValidatorjsRiot.prototype), "_parseRules", _assertThisInitialized(_this)).call(_assertThisInitialized(_this), rules);
 
     v.messages._setCustom(customMessages);
 
     v.setAttributeNames(attributeNames);
+    return _this;
   }
 
-  _prepareData(value, type) {
-    if (!value) {
+  _createClass(ValidatorjsRiot, [{
+    key: "_prepareData",
+    value: function _prepareData(value, type) {
+      if (!value) {
+        return value;
+      }
+
+      if (type && type.nodeValue == "number") {
+        return parseFloat(value);
+      }
+
       return value;
     }
+  }, {
+    key: "_prepareRule",
+    value: function _prepareRule(attributes) {
+      var rule = [];
 
-    if (type && type.nodeValue == "number") {
-      return parseFloat(value);
-    }
+      if (attributes.validate && attributes.validate.nodeValue) {
+        var value = attributes.validate.nodeValue;
 
-    return value;
-  }
-
-  _prepareRule(attributes) {
-    const rule = [];
-
-    if (attributes.validate && attributes.validate.nodeValue) {
-      const value = attributes.validate.nodeValue;
-
-      if (value.indexOf('|') >= 0) {
-        Array.prototype.push.apply(rule, value.split('|'));
-      } else {
-        rule.push(value);
+        if (value.indexOf('|') >= 0) {
+          Array.prototype.push.apply(rule, value.split('|'));
+        } else {
+          rule.push(value);
+        }
       }
+
+      if (attributes.required) {
+        rule.push('required');
+      }
+
+      if (attributes.min && attributes.min.nodeValue) {
+        rule.push("min:".concat(attributes.min.nodeValue));
+      }
+
+      if (attributes.max && attributes.max.nodeValue) {
+        rule.push("max:".concat(attributes.max.nodeValue));
+      }
+
+      if (attributes.pattern && attributes.pattern.nodeValue) {
+        rule.push("regex:".concat(attributes.pattern.nodeValue));
+      }
+
+      return rule;
     }
+  }, {
+    key: "_isTarget",
+    value: function _isTarget(ref) {
+      var target = this._option.target || [];
+      var except = this._option.except || [];
 
-    if (attributes.required) {
-      rule.push('required');
+      if (target.length > 0 && target.indexOf(ref) < 0) {
+        return false;
+      }
+
+      if (except.length > 0 && except.indexOf(ref) >= 0) {
+        return false;
+      }
+
+      return true;
     }
-
-    if (attributes.min && attributes.min.nodeValue) {
-      rule.push(`min:${attributes.min.nodeValue}`);
+  }], [{
+    key: "useLang",
+    value: function useLang(lang) {
+      Validator.useLang(lang);
     }
+  }]);
 
-    if (attributes.max && attributes.max.nodeValue) {
-      rule.push(`max:${attributes.max.nodeValue}`);
-    }
-
-    if (attributes.pattern && attributes.pattern.nodeValue) {
-      rule.push(`regex:${attributes.pattern.nodeValue}`);
-    }
-
-    return rule;
-  }
-
-  _isTarget(ref) {
-    const target = this._option.target || [];
-    const except = this._option.except || [];
-
-    if (target.length > 0 && target.indexOf(ref) < 0) {
-      return false;
-    }
-
-    if (except.length > 0 && except.indexOf(ref) >= 0) {
-      return false;
-    }
-
-    return true;
-  }
-
-  static useLang(lang) {
-    Validator.useLang(lang);
-  }
-
-}
+  return ValidatorjsRiot;
+}(Validator);
 
 module.exports = ValidatorjsRiot;
 
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,12 @@ module.exports = (env, argv) => {
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: ['babel-loader']
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
         }
       ]
     },
